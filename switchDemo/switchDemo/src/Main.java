3ce31eb17a57a03972ffd28ad80648236d30f154
Emir Karabulutoğlu <karabulutogluemir@gmail.com>
2022-10-13 11:34:05 +0300

    Versiyon 1.2

---
+++ switchDemo/switchDemo/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        String grade = "F";
+        switch (grade) {
+            case "A":
+                System.out.println("Helal Olsun Lan");
+            case "B":
+                System.out.println("Hadi Yine İyisin");
+            case "C":
+                System.out.println("Ah be Kral Olur Olur ");
+            case "D":
+                System.out.println("Napıyorsun lan kalacaktın az kalsın");
+            case "F":
+                System.out.println("Hahhh afferin sana , afferin sanaaa");
+        }
+
+    }
+}
