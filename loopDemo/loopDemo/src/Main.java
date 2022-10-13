41b8cfba2448f59d55b7b48f03cf21cc07f13ace
Emir Karabulutoğlu <karabulutogluemir@gmail.com>
2022-10-13 11:48:53 +0300

    Versiyon 1.4

---
+++ loopDemo/loopDemo/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        //For
+        System.out.println("Sistem verileri yükleniyor");
+        System.out.println("Geriye kalan süre : 10 Saniye");
+        for (int i=10;i>-1;i--)
+            System.out.println(i);
+
+        System.out.println("Sistem Verileri Başarıyla Yüklendi");
+    }
+}
